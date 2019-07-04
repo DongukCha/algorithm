8052cebe922d6d23d7407e205738fb3322288667
donguk <dongukcha0@gmail.com>
2019-07-04 14:36:49 +0900

    백준 8393번

--- 8393.js
+++ 8393.js
@@ -1,3 +1,11 @@
 var fs = require('fs');
 var input = fs.readFileSync('./input.txt').toString().split(' ');
 
+var n = parseInt(input[0]);
+var result = 0;
+
+for(var i = 1; i <= n; i++){
+    result += i;
+}
+
+console.log(result);
