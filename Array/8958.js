e78786bcdffa736f238e0eababed106357042d1d
donguk <dongukcha0@gmail.com>
2019-07-08 13:08:06 +0900

    백준 8958번

--- Array/8958.js
+++ Array/8958.js
@@ -1,28 +1,22 @@
 var fs = require('fs');
 var input = fs.readFileSync('./input.txt').toString().split('\n');
 
-var N = parseInt(input[0]);
-var arr = [];
+var n = parseInt(input[0]);
 
-
-for(var i = 1; i < input.length; i++){
-    arr.push(input[i]);
-}
-
-for(var i = 0; i < arr.length; i++){
+for(var i = 1; i <= n; i++){
+    var str = input[i];
     var score = 0;
-    var count = 0;
-    
-    for(var j = 0; j < arr[i].length; j++){
-       if(arr[i][j] === 'O'){
-           score++;
-           count += score ;
-       } else {
-           score = 0;
-       }
+    var combo = 0;
+
+    for(var j = 0, l = str.length; j < l; j++){
+        if(str[j] === 'O'){
+            combo++;
+            score += combo;
+        } else {
+            combo = 0;
+        }
     }
-    console.log(count);
-    count = 0;
+    console.log(score);
 }
 
 
