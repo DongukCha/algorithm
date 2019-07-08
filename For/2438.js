2c034d1933a0066b79b9ad2e61a57e2c03e2a2ab
donguk <dongukcha0@gmail.com>
2019-07-08 12:46:06 +0900

    백준 2438번

--- For/2438.js
+++ For/2438.js
@@ -2,10 +2,9 @@ var fs = require('fs');
 var input = fs.readFileSync('./input.txt').toString().split(' ');
 
 var n = parseInt(input[0]);
+var str = "";
 
 for(var i = 0; i < n; i++){
-    for(var j = 0; j < i + 1; j++){
-        console.log('*');
-    }
-    console.log('\n');
+    str += "*";
+    console.log(str);
 }
