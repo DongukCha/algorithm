a90152481c889092c489afd64b1c0576c2e564c7
donguk <dongukcha0@gmail.com>
2019-07-18 15:07:38 +0900

    백준 1463번

--- DP/1463.js
+++ DP/1463.js
@@ -1,28 +1,40 @@
 var input = require('fs').readFileSync('./input.txt').toString().split(' ');
 var n = parseInt(input);
-var arr = [];
+var dp = [];
+dp[0] = 0;
+dp[1] = 0;
 
-function dp(n){
-    if(n === 1){
-        return 0;
+for(var i = 2; i <= n; i++){
+    dp[i] = dp[i - 1] +1;
+    if(i % 2 === 0){
+        dp[i] = Math.min(dp[i], dp[i/2] + 1);
     }
-    if(arr[n] > 0) {
-        return arr[n];
+    if(i % 3 === 0){
+        dp[i] = Math.min(dp[i], dp[i/3] + 1);
     }
-    arr[n] = dp(n-1) + 1;
-    if(n%2 === 0){
-        var temp = dp(n/2) + 1;
-        if(arr[n] > temp){
-            arr[n] = temp;
-        }
-    }
-    if(n%3 === 0){
-        var tmp = dp(n/3) + 1;
-        if(arr[n] > tmp){
-            arr[n] = tmp;
-        }
-    }
-    return arr[n];
 }
 
-console.log(dp(n));
+console.log(dp[n]);
+
+// function dp(n){
+//     if(n === 1){
+//         return 0;
+//     }
+//     if(arr[n] > 0) {
+//         return arr[n];
+//     }
+//     arr[n] = dp(n-1) + 1;
+//     if(n%2 === 0){
+//         var temp = dp(n/2) + 1;
+//         if(arr[n] > temp){
+//             arr[n] = temp;
+//         }
+//     }
+//     if(n%3 === 0){
+//         var tmp = dp(n/3) + 1;
+//         if(arr[n] > tmp){
+//             arr[n] = tmp;
+//         }
+//     }
+//     return arr[n];
+// }
