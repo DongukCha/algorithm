0c6bf69c2924165aa79f8c8b0739bc5c7c423d3a
donguk <dongukcha0@gmail.com>
2019-07-23 16:24:59 +0900

    백준 2630번

---
+++ Divide&Conquer/2630.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var input = fs.readFileSync('./input.txt').toString().split('\n');
+var N = Number(input.shift());
+var map = [];
+var blue = 0;
+var white = 0;
+
+while (input.length) {
+    var arr = input.shift().split(' ').map(Number);
+    map.push(arr)
+}
+
+function countPaper(i, j, n) {
+    var c = true;
+    var sum = map[i][j];
+    if (n === 1) {
+        if (sum === 1) blue += 1;
+        if (sum === 0) white += 1;
+    } else {
+
+        for (var k = i; k < i + n; k++) {
+            for (var l = j; l < j + n; l++) {
+                if (sum !== map[k][l]) {
+                    c = false;
+                }
+            }
+        }
+
+        if (c) {
+            if (sum === 1) blue += 1;
+            if (sum === 0) white += 1;
+
+        } else {
+            var cnt = Math.floor(n / 2);
+            countPaper(i, j, cnt);
+            countPaper(i + cnt, j, cnt);
+            countPaper(i, j + cnt, cnt);
+            countPaper(i + cnt, j + cnt, cnt);
+        }
+    }
+}
+
+countPaper(0, 0, N);
+
+console.log(white);
+console.log(blue);
